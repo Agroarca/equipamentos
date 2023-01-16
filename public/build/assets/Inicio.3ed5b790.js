051025e88f5b1e47f176a4e81b51449d3c288505
Eduardo Pilati <pilatieduardo@gmail.com>
2023-01-16 18:11:42 -0300

    dockerfile + build

--- public/build/assets/Inicio.3ed5b790.js
+++
@@ -1,1 +0,0 @@
-import{A as e}from"./AdminLayout.8f9eb4b5.js";import{o,a,w as r,b as t}from"./app.1319b50a.js";import"./_plugin-vue_export-helper.b0c12f2d.js";const s=t("header",null,"Dashboard",-1),n=t("article",null,"content",-1),m={__name:"Inicio",setup(c){return(_,i)=>(o(),a(e,null,{default:r(()=>[s,n]),_:1}))}};export{m as default};
--- routes/channels.php
+++ routes/channels.php
@@ -13,18 +13,10 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
-
 Broadcast::channel('notificacoes.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
 Broadcast::channel('private-notificacoes.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
-});
-
-Broadcast::channel('teste', function () {
-    return true;
 });
