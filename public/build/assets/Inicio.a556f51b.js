bf34147d77bf95f4d105e46211b60d0ef86b02e0
Eduardo Pilati <pilatieduardo@gmail.com>
2023-02-23 20:38:36 -0300

    git ignore extensions

--- public/build/assets/Inicio.a556f51b.js
+++
@@ -1,1 +0,0 @@
-import{A as e}from"./AdminLayout.66c9bba9.js";import{a as o,w as a,o as r,b as t}from"./app.a897a209.js";import"./_plugin-vue_export-helper.0e486f82.js";const s=t("header",null,"Dashboard",-1),n=t("article",null,"content",-1),m={__name:"Inicio",setup(c){return(_,i)=>(r(),o(e,null,{default:a(()=>[s,n]),_:1}))}};export{m as default};
