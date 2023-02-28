b5246b0f604546c981049228bf7750d5f9b3d16e
Eduardo Pilati <pilatieduardo@gmail.com>
2023-02-28 15:42:15 -0300

    Build assets

--- public/build/assets/Inicio.8b2beff6.js
+++
@@ -1,1 +0,0 @@
-import{S as t}from"./SiteLayout.78a2a4df.js";import{a as o,w as e,o as a,b as r}from"./app.ce7f9479.js";import"./_plugin-vue_export-helper.bce54cdc.js";const s=r("p",null,"inicio",-1),u={__name:"Inicio",setup(c){return(i,n)=>(a(),o(t,null,{default:e(()=>[s]),_:1}))}};export{u as default};
