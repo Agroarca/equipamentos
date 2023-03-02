46d4e002900ef972aed1837c51e2cbdc8a4de906
paulohpps <paulohpps13@gmail.com>
2023-03-02 13:44:11 -0300

    Componente SelectAjax Finalizado

--- resources/js/Pages/Inicio.vue
+++ resources/js/Pages/Inicio.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
 import SiteLayout from '@/Layouts/SiteLayout.vue'
-import SelectAjax from '@/Components/Util/SelectAjax.vue'
 
-let vModel = 10
-const options = [{ id: 10, texto: 'Selecionado' }, { id: 20, texto: 'Não Selecionado' }]
 </script>
 
 <template>
     <SiteLayout>
         <p>inicio</p>
-        <SelectAjax v-model="vModel" :href="route('site.modelos.pesquisar')" :options="options" />
     </SiteLayout>
 </template>
