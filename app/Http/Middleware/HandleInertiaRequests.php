dcf7f2f4072dce43bdc1075ac88ff4eac3a7d4e7
paulohpps <paulohpps13@gmail.com>
2023-03-16 17:17:52 -0300

    Correção erro do link do dashboard não aparece

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -15,10 +15,7 @@ class HandleInertiaRequests extends Middleware
 
         return array_merge(parent::share($request), [
             'auth' => [
-                'user' => [
-                    'id' => $request->user()?->id,
-                    'name' => $request->user()?->name,
-                ],
+                'user' => $request->user()
             ],
             'admin' => [
                 'menus' => MenuService::getAuthMenus(),
