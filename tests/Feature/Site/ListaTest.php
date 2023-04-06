62c859188e31f29b72785ce34c0a65d2f757fedf
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-04-06 10:48:20 -0300

    Testes refatorados

--- tests/Feature/Site/ListaTest.php
+++ tests/Feature/Site/ListaTest.php
@@ -3,8 +3,11 @@
 namespace Tests\Feature;
 
 use App\Models\Equipamentos\Cadastro\Categoria;
+use App\Models\Equipamentos\Cadastro\Equipamento;
 use App\Models\Equipamentos\Cadastro\Marca;
+use App\Models\Equipamentos\Cadastro\Modelo;
 use App\Models\Equipamentos\Lista\Lista;
+use App\Models\Equipamentos\Lista\ProdutoLista;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Inertia\Testing\AssertableInertia;
 use Tests\TestCase;
@@ -15,33 +18,82 @@ class ListaTest extends TestCase
 
     public function testPodeAcessarCategorias(): void
     {
+        Equipamento::factory()->count(4)->create();
+
         $response = $this->actingAs($this->getAdmin())
             ->get('/categoria');
         $response->assertStatus(200);
         $response->assertInertia(fn (AssertableInertia $page) => $page
-            ->component('Site/Lista/Categoria'));
+            ->component('Site/Lista/Categoria')
+            ->has('equipamentos.data', 4)
+            ->has('categoria'));
     }
 
     public function testPodeAcessarCategoria(): void
     {
         $categoria = Categoria::factory()->create();
+        Equipamento::factory()->count(4)->create([
+            'categoria_id' => $categoria->id,
+        ]);
 
         $response = $this->actingAs($this->getAdmin())
             ->get("/categoria/{$categoria->id}");
         $response->assertStatus(200);
         $response->assertInertia(fn (AssertableInertia $page) => $page
-            ->component('Site/Lista/Categoria'));
+            ->component('Site/Lista/Categoria')
+            ->has('equipamentos.data', 4)
+            ->has('categoria')
+            ->where('categoria.id', $categoria->id));
+    }
+
+    public function testPodeAcessarCetegoriaRetornaApenasCategoriasFilhasDela(): void
+    {
+        $categoria = Categoria::factory()->create();
+        $categoriaFilha = Categoria::factory()->create([
+            'categoria_mae_id' => $categoria->id,
+        ]);
+        Equipamento::factory()->count(3)->create([
+            'categoria_id' => $categoriaFilha->id,
+        ]);
+
+        Equipamento::factory()->count(3)->create([
+            'categoria_id' => $categoria->id,
+        ]);
+
+        Equipamento::factory()->count(4)->create();
+
+        $response = $this->actingAs($this->getAdmin())
+            ->get("/categoria/{$categoria->id}");
+        $response->assertStatus(200);
+        $response->assertInertia(fn (AssertableInertia $page) => $page
+            ->component('Site/Lista/Categoria')
+            ->has('equipamentos.data', 6)
+            ->has('categoria')
+            ->where('categoria.id', $categoria->id));
     }
 
     public function testPodeAcessarMarca(): void
     {
-        $marca = Marca::factory()->create();
+
+        $modelo1 = Modelo::factory()->create();
+        $modelo2 = Modelo::factory()->create();
+
+        Equipamento::factory()->count(4)->create([
+            'modelo_id' => $modelo1->id,
+        ]);
+
+        Equipamento::factory()->count(4)->create([
+            'modelo_id' => $modelo2->id,
+        ]);
 
         $response = $this->actingAs($this->getAdmin())
-            ->get("/marca/{$marca->id}");
+            ->get("/marca/{$modelo1->marca_id}");
         $response->assertStatus(200);
         $response->assertInertia(fn (AssertableInertia $page) => $page
-            ->component('Site/Lista/Marca'));
+            ->component('Site/Lista/Marca')
+            ->has('equipamentos.data', 4)
+            ->has('marca')
+            ->where('marca.id', $modelo1->marca_id));
     }
 
     public function testNaoPodeAcessarMarcaInexistente(): void
@@ -60,23 +112,47 @@ class ListaTest extends TestCase
 
     public function testPodeAcessarListaComId(): void
     {
-        $lista = Lista::factory()->create();
+        $lista1 = Lista::factory()->create();
+        $lista2 = Lista::factory()->create();
+
+        ProdutoLista::factory()->count(4)->create([
+            'lista_id' => $lista1->id,
+        ]);
+
+        ProdutoLista::factory()->count(4)->create([
+            'lista_id' => $lista2->id,
+        ]);
 
         $response = $this->actingAs($this->getAdmin())
-            ->get("/lista/{$lista->id}");
+            ->get("/lista/{$lista1->id}");
         $response->assertStatus(200);
         $response->assertInertia(fn (AssertableInertia $page) => $page
-            ->component('Site/Lista/Lista'));
+            ->component('Site/Lista/Lista')
+            ->has('equipamentos.data', 4)
+            ->has('lista')
+            ->where('lista.id', $lista1->id));
     }
 
     public function testPodeAcessarListaComSlug(): void
     {
-        $lista = Lista::factory()->create();
+        $lista1 = Lista::factory()->create();
+        $lista2 = Lista::factory()->create();
+
+        ProdutoLista::factory()->count(4)->create([
+            'lista_id' => $lista1->id,
+        ]);
+
+        ProdutoLista::factory()->count(4)->create([
+            'lista_id' => $lista2->id,
+        ]);
 
         $response = $this->actingAs($this->getAdmin())
-            ->get("/lista/{$lista->slug}");
+            ->get("/lista/{$lista1->slug}");
         $response->assertStatus(200);
         $response->assertInertia(fn (AssertableInertia $page) => $page
-            ->component('Site/Lista/Lista'));
+            ->component('Site/Lista/Lista')
+            ->has('equipamentos.data')
+            ->has('lista')
+            ->where('lista.id', $lista1->id));
     }
 }
