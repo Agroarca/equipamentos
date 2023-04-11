9118e10b9aa7420b8ef79c3ae6b082e5bf94377a
Eduardo Pilati <pilatieduardo@gmail.com>
2023-04-10 23:02:33 -0300

    Ajuste script deploy prod

--- docker/prod/deploy.sh
+++ docker/prod/deploy.sh
@@ -1,2 +1,12 @@
-docker compose build .
-docker compose restart
+#!/bin/bash
+
+cd /var/www/equipamentos/projeto
+
+git checkout main --force
+git pull
+
+cd /var/www/equipamentos
+
+sudo docker compose build
+sudo docker compose down
+sudo docker compose up -d
