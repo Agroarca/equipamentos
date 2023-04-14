6b538a4e0d1e5ba5ae91862f13e2b70d8b3e637d
Eduardo Pilati <pilatieduardo@gmail.com>
2023-04-14 12:37:59 -0300

    Refatoração Docker Test

--- docker/test/docker_entrypoint_queue.sh
+++ docker/test/docker_entrypoint_queue.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-composer install
-php artisan cache:clear
-php artisan route:clear
 php artisan queue:listen
--- docker/test/docker_entrypoint_web.sh
+++ docker/test/docker_entrypoint_web.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-composer install
 php artisan migrate --force
-php artisan cache:clear
-php artisan route:clear
 php artisan serve --host 0.0.0.0
