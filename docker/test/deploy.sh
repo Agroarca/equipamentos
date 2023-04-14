489ad16226dec52040b9f7d6fc9c7d353f28f41a
Eduardo Pilati <pilatieduardo@gmail.com>
2023-04-14 12:40:01 -0300

    criação deploy.sh

---
+++ docker/test/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /var/www/equipamentos/projeto
+
+git checkout main --force
+git pull
+
+sudo chmod +x docker/test/docker_entrypoint_web.sh
+sudo chmod +x docker/test/docker_entrypoint_queue.sh
+
+cd /var/www/equipamentos
+
+sudo docker compose build
+sudo docker compose down
+sudo docker compose up -d
+
+sudo chmod +x deploy.sh
