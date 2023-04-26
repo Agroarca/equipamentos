3645fc16bfdb6d61e0c4bf87d3fa79c95b283306
Eduardo Pilati <pilatieduardo@gmail.com>
2023-04-25 21:47:57 -0300

    Remover soft deletes lista de produtos

--- app/Models/Equipamentos/Lista/Lista.php
+++ app/Models/Equipamentos/Lista/Lista.php
@@ -5,11 +5,10 @@ namespace App\Models\Equipamentos\Lista;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Lista extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $table = 'listas';
 
