951f2bc27a26c8c2e9f9de56491a1e7dce107425
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-04-29 16:57:58 -0300

    Componente senha criado

---
+++ resources/js/Componentes/Site/Senha.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import FormError from '../Layout/Forms/FormError.vue'
+
+let props = defineProps({
+    modelValue: null,
+    error: String,
+})
+
+const emit = defineEmits<{(e: 'update:modelValue', value: string): void}>()
+
+let mostrarSenha = ref(false)
+
+const value = computed({
+    get() {
+        return props.modelValue
+    },
+    set(val: string) {
+        emit('update:modelValue', val)
+    },
+})
+
+</script>
+
+<template>
+    <div>
+        <label for="password">Senha</label>
+        <div class="input-group">
+            <input id="password" v-model="value" class="form-control" :type="mostrarSenha ? 'text' : 'password'" required autocomplete="current-password">
+            <span id="basic-addon2" class="input-group-text" @click="mostrarSenha = !mostrarSenha">
+                <input id=" mostrar_senha" v-model="mostrarSenha" class="form-check-input" type="checkbox">
+                <label class="form-check-label ps-2" for="mostrar_senha">Mostrar senha</label>
+            </span>
+        </div>
+        <FormError :error="error" />
+    </div>
+</template>
