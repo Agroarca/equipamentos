28c852eda47081cf12edbc7783bba6fe29da6f52
paulohpps <paulohpps13@gmail.com>
2023-05-11 16:32:31 -0300

    Remoção use desnecessário

--- app/Http/Controllers/Site/EquipamentoController.php
+++ app/Http/Controllers/Site/EquipamentoController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Site;
 
 use App\Enums\Equipamentos\Cadastro\StatusEquipamento;
-use App\Enums\Usuario\TipoUsuario;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Equipamentos\Cadastro\EquipamentoRequest;
 use App\Http\Requests\Admin\Equipamentos\Caracteristicas\CaracteristicasValorRequest;
