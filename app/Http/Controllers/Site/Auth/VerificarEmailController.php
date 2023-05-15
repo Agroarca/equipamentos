cf815c1f8135e9aeca5c641c1400c56171903b0f
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-05-15 15:11:15 -0300

    RateLimiter configurado

--- app/Http/Controllers/Site/Auth/VerificarEmailController.php
+++ app/Http/Controllers/Site/Auth/VerificarEmailController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Site\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Inertia\Inertia;
 
 class VerificarEmailController extends Controller
@@ -17,7 +18,16 @@ class VerificarEmailController extends Controller
 
     public function reenviarEmail(Request $request)
     {
+        if ($request->user()->hasVerifiedEmail()) {
+            return abort(403, 'Sua conta já foi verificada.');
+        }
+        if (RateLimiter::tooManyAttempts('verificar-email' . $request->user()->id, 1)) {
+            $segundos = RateLimiter::availableIn('verificar-email' . $request->user()->id);
+
+            return abort(403, 'Muitas tentativas, tente novamente em ' . $segundos . ' seconds.');
+        }
         $request->user()->sendEmailVerificationNotification();
+        RateLimiter::hit('verificar-email' . $request->user()->id);
         return redirect()->route('site.perfil');
     }
 }
