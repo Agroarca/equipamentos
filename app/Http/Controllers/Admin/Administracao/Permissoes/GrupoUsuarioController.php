e211a706eb39c830b5bcad212e094c81de315b63
Eduardo Pilati <pilatieduardo@gmail.com>
2023-05-15 17:33:20 -0300

    Testes Grupo Usuario

--- app/Http/Controllers/Admin/Administracao/Permissoes/GrupoUsuarioController.php
+++ app/Http/Controllers/Admin/Administracao/Permissoes/GrupoUsuarioController.php
@@ -20,25 +20,23 @@ class GrupoUsuarioController extends Controller
 
     public function inicio(Grupo $grupo): mixed
     {
-        Gate::authorize(GrupoUsuario::class, 'ver');
+        Gate::authorize('ver', GrupoUsuario::class);
         $usuarios = $grupo->usuarios()->paginate(10);
         return Inertia::render('Admin/Administracao/Permissoes/GrupoUsuario/Inicio', compact('grupo', 'usuarios'));
     }
 
     public function adicionar(Request $request, Grupo $grupo): mixed
     {
-        Gate::authorize(GrupoUsuario::class, 'editar');
-        $this->permissoesService->adicionarUsuarioGrupo($request->usuario_id, $grupo->id);
+        Gate::authorize('adicionar', GrupoUsuario::class);
+        $this->permissoesService->adicionarUsuarioGrupo($request->usuario_id, $grupo);
 
         return redirect()->route('admin.administracao.permissoes.grupo.usuarios', $grupo->id);
     }
 
     public function excluir(Grupo $grupo, Usuario $usuario): mixed
     {
-        Gate::authorize(GrupoUsuario::class, 'editar');
-        GrupoUsuario::where('grupo_id', $grupo->id)
-            ->where('usuario_id', $usuario->id)
-            ->delete();
+        Gate::authorize('excluir', GrupoUsuario::class);
+        $this->permissoesService->removerUsuarioGrupo($usuario->id, $grupo);
 
         return redirect()->route('admin.administracao.permissoes.grupo.usuarios', $grupo->id);
     }
---
+++ tests/Feature/Admin/Administracao/Permissoes/GrupoUsuarioTest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Tests\Feature\Admin\Administracao\Permissoes;
+
+use App\Models\Administracao\Permissoes\Grupo;
+use App\Models\Administracao\Permissoes\GrupoUsuario;
+use App\Models\Usuario;
+use App\Services\Administracao\Permissoes\PermissoesService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Inertia\Testing\AssertableInertia;
+use Tests\TestCase;
+
+class GrupoUsuarioTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testPodeAcessar(): void
+    {
+        $grupo = Grupo::factory()->create();
+
+        $usuario = $this->getAdmin();
+        $this->adicionarPermissoes($usuario, ['administracao.permissoes.grupo_usuario:ver']);
+
+        $response = $this->actingAs($usuario)
+            ->get("/admin/administracao/permissoes/grupo/$grupo->id/usuarios");
+
+        $response->assertStatus(200);
+    }
+
+    public function testPodeAcessarComDados(): void
+    {
+        $grupo = Grupo::factory()->create();
+        $usuarios = Usuario::factory()->count(4)->create();
+
+        $service = app(PermissoesService::class);
+        foreach ($usuarios as $usuario) {
+            $service->adicionarUsuarioGrupo($usuario->id, $grupo);
+        }
+
+        $usuario = $this->getAdmin();
+        $this->adicionarPermissoes($usuario, ['administracao.permissoes.grupo_usuario:ver']);
+
+        $response = $this->actingAs($usuario)
+            ->get("/admin/administracao/permissoes/grupo/$grupo->id/usuarios");
+
+        $response->assertStatus(200);
+        $response->assertInertia(fn (AssertableInertia $page) => $page
+            ->component('Admin/Administracao/Permissoes/GrupoUsuario/Inicio')
+            ->where('grupo.id', $grupo->id)
+            ->has('usuarios')
+            ->has('usuarios.data', 4));
+    }
+
+    public function testPodeAdicionarUsuario(): void
+    {
+        $grupo = Grupo::factory()->create();
+        $usuario = Usuario::factory()->create();
+
+        $usuario = $this->getAdmin();
+        $this->adicionarPermissoes($usuario, ['administracao.permissoes.grupo_usuario:adicionar']);
+
+        $response = $this->actingAs($usuario)
+            ->post("/admin/administracao/permissoes/grupo/$grupo->id/usuarios/adicionar", [
+                'usuario_id' => $usuario->id,
+            ]);
+
+        $response->assertRedirectToRoute('admin.administracao.permissoes.grupo.usuarios', $grupo->id);
+        $this->assertDatabaseHas(app(GrupoUsuario::class)->getTable(), [
+            'grupo_id' => $grupo->id,
+            'usuario_id' => $usuario->id,
+        ]);
+    }
+
+    public function testPodeExcluirUsuario(): void
+    {
+        $grupo = Grupo::factory()->create();
+        $usuario = Usuario::factory()->create();
+        app(PermissoesService::class)->adicionarUsuarioGrupo($usuario->id, $grupo);
+
+        $admin = $this->getAdmin();
+        $this->adicionarPermissoes($admin, ['administracao.permissoes.grupo_usuario:excluir']);
+
+        $response = $this->actingAs($admin)
+            ->get("/admin/administracao/permissoes/grupo/$grupo->id/usuarios/$usuario->id/excluir");
+
+        $response->assertRedirectToRoute('admin.administracao.permissoes.grupo.usuarios', $grupo->id);
+        $this->assertDatabaseMissing(app(GrupoUsuario::class)->getTable(), [
+            'grupo_id' => $grupo->id,
+            'usuario_id' => $usuario->id,
+        ]);
+    }
+
+    public function testNaoPodeAcessarSemPermissao(): void
+    {
+        $grupo = Grupo::factory()->create();
+
+        $response = $this->actingAs($this->getAdmin())
+            ->get("/admin/administracao/permissoes/grupo/$grupo->id/usuarios");
+
+        $response->assertStatus(403);
+    }
+
+    public function testNaoPodeAdicionarUsuarioSemPermissao(): void
+    {
+        $grupo = Grupo::factory()->create();
+        $usuario = Usuario::factory()->create();
+
+        $response = $this->actingAs($this->getAdmin())
+            ->post("/admin/administracao/permissoes/grupo/$grupo->id/usuarios/adicionar", [
+                'usuario_id' => $usuario->id,
+            ]);
+
+        $response->assertStatus(403);
+        $this->assertDatabaseMissing(app(GrupoUsuario::class)->getTable(), [
+            'grupo_id' => $grupo->id,
+            'usuario_id' => $usuario->id,
+        ]);
+    }
+
+    public function testNaoPodeExcluirUsuarioSemPermissao(): void
+    {
+        $grupo = Grupo::factory()->create();
+        $usuario = Usuario::factory()->create();
+
+        app(PermissoesService::class)->adicionarUsuarioGrupo($usuario->id, $grupo);
+
+        $response = $this->actingAs($this->getAdmin())
+            ->get("/admin/administracao/permissoes/grupo/$grupo->id/usuarios/$usuario->id/excluir");
+
+        $response->assertStatus(403);
+        $this->assertDatabaseHas(app(GrupoUsuario::class)->getTable(), [
+            'grupo_id' => $grupo->id,
+            'usuario_id' => $usuario->id,
+        ]);
+    }
+}
