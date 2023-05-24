eb9c96704d88117f6479b6ed8ceb95c1fdffcc53
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-05-24 20:09:05 -0300

    Correção de tempo em horas para minutos

--- app/Notifications/Usuario/VerificarEmail.php
+++ app/Notifications/Usuario/VerificarEmail.php
@@ -31,7 +31,7 @@ class VerificarEmail extends Notification implements ShouldQueue
     {
         return URL::temporarySignedRoute(
             'auth.verificar.email',
-            Carbon::now()->addHour(Config::get('auth.verification.expire', 12)),
+            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 720)),
             [
                 'id' => $notifiable->getKey(),
                 'hash' => sha1($notifiable->getEmailForVerification()),
