d6aae672c556681e7c53374030fe252c8aed4ff0
Eduardo Pilati <pilatieduardo@gmail.com>
2023-05-30 17:48:03 -0300

    atualizar deploy

--- docker/prod/deploy.sh
+++ docker/prod/deploy.sh
@@ -2,24 +2,33 @@
 
 cd /var/www/equipamentos/projeto
 
+# atualizar git
 git reset --hard
 git pull
 
+sudo chmod +x deploy.sh
 sudo chmod +x docker/prod/docker_entrypoint_web.sh
 sudo chmod +x docker/prod/docker_entrypoint_queue.sh
 
+# build elastic-apm
 cd /var/www/equipamentos/projeto/docker/prod/elastic-apm
-
 sudo docker build -t elastic-apm:latest . --progress plain
 
+# build php
 cd /var/www/equipamentos
-
 sudo docker compose build --progress plain
+
+# remove equipamentos
 sudo docker compose down
+
+#remove nginx (necessário para atualizar os estáticos)
+cd /var/www/nginx
+sudo docker compose down
+
+# iniciar equipametnos
+cd /var/www/equipamentos
 sudo docker compose up -d
 
-sudo chmod +x deploy.sh
-
+#iniciar nginx
 cd /var/www/nginx
-
-sudo docker compose restart
+sudo docker compose up -d
