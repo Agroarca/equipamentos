64b542082c1b1e0d6e3f954f140d5c63ea68246a
Eduardo Pilati <pilatieduardo@gmail.com>
2023-05-31 13:10:57 -0300

    ajuste permissao

--- docker/prod/deploy.sh
+++ docker/prod/deploy.sh
@@ -6,7 +6,6 @@ cd /var/www/equipamentos/projeto
 git reset --hard
 git pull
 
-sudo chmod +x deploy.sh
 sudo chmod +x docker/prod/docker_entrypoint_web.sh
 sudo chmod +x docker/prod/docker_entrypoint_queue.sh
 
@@ -21,17 +20,20 @@ sudo docker compose build --progress plain
 # remove equipamentos
 sudo docker compose down
 
-#remove nginx (necessário para atualizar os estáticos)
+# remove nginx (necessário para atualizar os estáticos)
 cd /var/www/nginx
 sudo docker compose down
 
-#excluir volume de estáticos
+# excluir volume de estáticos
 sudo docker volume rm equipamentos-public
 
 # iniciar equipametnos
 cd /var/www/equipamentos
 sudo docker compose up -d
 
-#iniciar nginx
+# iniciar nginx
 cd /var/www/nginx
 sudo docker compose up -d
+
+# permissão para executar o deploy
+sudo chmod +x deploy.sh
