58e0fad2fcef0eb437c8ec4dc47871810889d002
Eduardo Pilati <pilatieduardo@gmail.com>
2023-05-31 15:52:17 -0300

    Correção Caracteristicas

--- resources/js/Pages/Site/Equipamento/Cadastrar/Caracteristicas.vue
+++ resources/js/Pages/Site/Equipamento/Cadastrar/Caracteristicas.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useForm } from '@inertiajs/vue3'
+import { fromPairs, map } from 'lodash'
 import SiteLayout from '@/Layouts/SiteLayout.vue'
 import FormError from '@/Componentes/Layout/Forms/FormError.vue'
 import Navegacao from './Componentes/Navegacao.vue'
@@ -10,10 +11,9 @@ const props = defineProps({
     equipamento: Object,
 })
 
-const valores = []
-props.caracteristicas.forEach((caracteristica) => {
-    valores[`carac-${caracteristica.id}`] = caracteristica.valor
-})
+const valores = fromPairs(map(props.caracteristicas, (caracteristica) => [
+    `carac-${caracteristica.id}`, caracteristica.valor,
+]))
 
 const form = useForm(valores)
 
