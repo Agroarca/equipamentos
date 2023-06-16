e7e638f31d040c218c6568ede51733de3be7c5cf
Eduardo Pilati <pilatieduardo@gmail.com>
2023-06-16 14:18:13 -0300

    add elastic RUM

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@ import '../css/app.css'
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { ApmVuePlugin } from '@elastic/apm-rum-vue'
 
 const appName = 'Agroarca'
 
@@ -18,6 +19,11 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(ApmVuePlugin, {
+                serviceName: 'equipamentos',
+                serverUrl: 'https://agroarca.com.br:2083',
+                active: import.meta.env.VITE_APM_HABILITADO,
+            })
             .mount(el)
     },
 })
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,5 +7,3 @@ import 'bootstrap'
 window._ = _
 window.axios = axios
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
-// import * as bootstrap from 'bootstrap';
-// window.bootstrap = bootstrap;
