ddb130397b24d59b4da1e87b2199b77d00a6cc7b
Eduardo Pilati <pilatieduardo@gmail.com>
2023-06-22 14:57:17 -0300

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+    <link rel="alternate icon" type="image/png" href="/favicon.png">
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+
     <!-- app -->
     @vite('resources/js/app.js')
     <!-- Scripts -->
