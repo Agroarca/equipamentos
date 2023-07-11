595f7da3430ae4876af52bfd478208f2d1f075b1
Eduardo Pilati <pilatieduardo@gmail.com>
2023-07-11 12:50:35 -0300

    ajuste na pesquisa de cidades

--- app/Http/Controllers/Admin/PesquisaController.php
+++ app/Http/Controllers/Admin/PesquisaController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Admin;
 use App\Enums\Usuario\TipoUsuario;
 use App\Http\Controllers\Controller;
 use App\Models\Cadastro\Cidade;
+use App\Models\Cadastro\Estado;
 use App\Models\Usuario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PesquisaController extends Controller
 {
@@ -25,19 +27,24 @@ class PesquisaController extends Controller
         return response()->json($usuarios);
     }
 
-    public function cidade(Request $request)
+    public function cidade(Request $request): mixed
     {
-        $cidades = Cidade::whereFullText('nome', $request->input('termo'))
-            ->orWhere('nome', 'like', '%' . $request->input('termo') . '%')
+        $termo = $request->input('termo');
+
+        $cidades = DB::table(app(Cidade::class)->getTable() . ' as cidades')
+            ->join(app(Estado::class)->getTable() . ' as estados', 'estados.id', '=', 'estado_id')
+
+            ->select('cidades.id as id')
+            ->selectRaw("concat(cidades.nome, '/', estados.uf) as texto")
+
+            ->whereFullText('cidades.nome', $termo)
+            ->orWhere('cidades.nome', 'like', '%' . $termo . '%')
+
+            ->orderByRaw("MATCH (`cidades`.`nome`) AGAINST ('$termo' IN NATURAL LANGUAGE MODE)
+                + case when ('$termo' like concat('%', estados.uf)) then 1 else 0 end desc")
+
             ->take(10)
             ->get();
-
-        $cidades = $cidades->map(function ($cidade) {
-            return [
-                'id' => $cidade->id,
-                'texto' => $cidade->display_name,
-            ];
-        });
 
         return response()->json($cidades);
     }
