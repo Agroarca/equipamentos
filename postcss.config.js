db1dd2e2c67e2129b54db2ca6b7f751020bacb6b
Eduardo Pilati <pilatieduardo@gmail.com>
2023-07-31 21:03:13 -0300

    Correção problema mixins (#460)

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable global-require */
 module.exports = {
     plugins: [
-        require('postcss-mixins'),
         require('postcss-simple-vars'),
         require('postcss-nested'),
         require('autoprefixer'),
