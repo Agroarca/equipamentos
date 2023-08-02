db156ca3ae76738309f654217726db2cdfb54424
Eduardo Pilati <pilatieduardo@gmail.com>
2023-08-02 14:54:31 -0300

    Ajuste falta classe imagens lista equipamentos

--- resources/js/Pages/Site/Lista/Componentes/Equipamento.vue
+++ resources/js/Pages/Site/Lista/Componentes/Equipamento.vue
@@ -11,7 +11,7 @@ const props = defineProps({
     <Link class="produto" :href="`/equipamento/${equipamento.id}`">
         <picture>
             <source v-if="equipamento.imagens[0]?.url_secundario !== null" :srcset="equipamento.imagens[0]?.url_secundario" type="image/webp">
-            <img :src="equipamento.imagens[0]?.url ?? '/img/Placeholder.png'" alt="Imagem do equipamento cadastrada pelo anunciante." width="800" height="600">
+            <img class="imagem" :src="equipamento.imagens[0]?.url ?? '/img/Placeholder.png'" alt="Imagem do equipamento cadastrada pelo anunciante." width="800" height="600">
         </picture>
         <span class="titulo flex-fill">{{ equipamento.titulo }}</span>
         <Formatacao class="valor" tipo="preco" :valor="equipamento.valor" />
